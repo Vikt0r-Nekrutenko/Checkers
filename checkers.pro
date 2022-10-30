e3671c3c9573817e1ee0f224739aa011a102089e
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-30 12:31:42 +0200

    001

--- checkers.pro
+++ checkers.pro
@@ -8,5 +8,9 @@ SOURCES += \
 
 LIBS += -L$$PWD/../stf/release/ -lstf
 
-INCLUDEPATH += $$PWD/../stf
-DEPENDPATH += $$PWD/../stf
+INCLUDEPATH += $$PWD/../stf \
+                $$PWD/../stf/smv \
+                $$PWD/../stf/sdb \
+DEPENDPATH += $$PWD/../stf \
+                $$PWD/../stf/smv \
+                $$PWD/../stf/sdb \
--- main.cpp
+++ main.cpp
@@ -1,9 +1,86 @@
 #include <window.hpp>
+#include <iview.hpp>
+#include <imodel.hpp>
+
+stf::sdb::DynamicFieldsAllocator _cellAlloc;
+
+class BoardCell
+{
+public:
+    virtual uint8_t view() const { return 'e'; }
+
+    void* operator new(size_t size)
+    {
+      void* ptr;
+      if(offset == 0xffffffff) {
+        ptr = _cellAlloc.allocate(size);
+      } else {
+        ptr = _cellAlloc.mem() + offset;
+      }
+      return ptr;
+    }
+
+    static uint32_t offset;
+};
+
+uint32_t BoardCell::offset = 0xffffffff;
+
+class CheckerCell : public BoardCell
+{
+public:
+    uint8_t view() const final { return 'o'; }
+};
+
+class QueenCheckerCell : public BoardCell
+{
+public:
+    uint8_t view() const final { return 'O'; }
+};
+
+class GameModel : public stf::smv::BaseModel
+{
+public:
+    GameModel() : stf::smv::BaseModel() {
+        for(auto it = board.begin(); it != board.end(); ++it) {
+            *it = new BoardCell();
+        }
+        board.at(0) = new CheckerCell();
+        board.at(1) = new QueenCheckerCell();
+    }
+
+    BoardCell* get(const stf::Vec2d& pos) { return board.at(Size.x * pos.y + pos.x); }
+
+    stf::Vec2d Size { 8, 8 };
+    std::vector<BoardCell*> board = std::vector<BoardCell*>(Size.x * Size.y);
+};
+
+class GameView : public stf::smv::IView
+{
+public:
+    GameView(GameModel *model)
+        : IView(model)
+    {}
+
+    void show(stf::Renderer &renderer) final
+    {
+        GameModel *model = static_cast<GameModel*>(m_model);
+        for(int y = 0; y < model->Size.y; ++y) {
+            for(int x = 0; x < model->Size.x; ++x) {
+                renderer.drawPixel({x,y+2}, model->get({x,y})->view());
+            }
+        }
+    }
+};
 
 class Game : public stf::Window
 {
+    GameModel gameModel = GameModel();
+    GameView gameView = GameView(&gameModel);
+    stf::smv::IView *currentView = &gameView;
+
     bool onUpdate(const float) final
     {
+        currentView->show(renderer);
         return true;
     }
 
