8b3e95ecede1e571c12048abc9d8251dfee8b455
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-10-31 14:01:07 +0200

    007

--- main.cpp
+++ main.cpp
@@ -1,46 +1,58 @@
 #include <window.hpp>
 #include <iview.hpp>
 #include <imodel.hpp>
+#include <iostream>
 
 stf::sdb::DynamicFieldsAllocator _cellAlloc;
+
+struct Cursor;
 
 class BoardCell
 {
 public:
-    virtual uint8_t view() const { return 'e'; }
+    inline virtual uint8_t view() const { return 'e'; }
+    virtual bool onPlacementHandler(std::vector<BoardCell*>& board, Cursor& cursor);
 
-    void* operator new(size_t size)
-    {
-      void* ptr;
-      if(offset == 0xffffffff) {
-        ptr = _cellAlloc.allocate(size);
-      } else {
-        ptr = _cellAlloc.mem() + offset;
-      }
-      return ptr;
-    }
-
-    static uint32_t offset;
+    void* operator new(size_t size) { return _cellAlloc.allocate(size); }
+    bool operator !=(const BoardCell& cell) const { return view() != cell.view(); }
+    bool operator ==(const BoardCell& cell) const { return view() != cell.view(); }
 };
-
-uint32_t BoardCell::offset = 0xffffffff;
 
 class CheckerCell : public BoardCell
 {
 public:
-    uint8_t view() const final { return 'o'; }
+    inline uint8_t view() const final { return 'o'; }
 };
 
 class QueenCheckerCell : public BoardCell
 {
 public:
-    uint8_t view() const final { return 'O'; }
+    inline uint8_t view() const final { return 'O'; }
 };
+
+template<typename T> class BoardCellCreator
+{
+public:
+    T *create() { return cell = cell == nullptr ? new T() : cell; }
+    T *cell = nullptr;
+};
+
+class BoardCellFactory
+{
+public:
+    static BoardCellCreator<BoardCell> boardCell;
+    static BoardCellCreator<CheckerCell> checkerCell;
+    static BoardCellCreator<QueenCheckerCell> queenCheckerCell;
+};
+
+BoardCellCreator<BoardCell> BoardCellFactory::boardCell = BoardCellCreator<BoardCell>();
+BoardCellCreator<CheckerCell> BoardCellFactory::checkerCell = BoardCellCreator<CheckerCell>();
+BoardCellCreator<QueenCheckerCell> BoardCellFactory::queenCheckerCell = BoardCellCreator<QueenCheckerCell>();
 
 struct Selector
 {
     stf::Vec2d pos { 0, 0 };
-    BoardCell *cell = new BoardCell;
+    BoardCell *cell = BoardCellFactory::boardCell.create();
 };
 
 struct Cursor
@@ -49,18 +61,31 @@ struct Cursor
     Selector destinationCell;
 };
 
+bool BoardCell::onPlacementHandler(std::vector<BoardCell*>& board, Cursor& cursor)
+{
+    Selector &sc = cursor.selectorCell;
+    Selector &dc = cursor.destinationCell;
+    BoardCell* cell = board.at(8 * cursor.selectorCell.pos.y + cursor.selectorCell.pos.x);
+    if(dc.pos.diff(sc.pos) <= 1.5f) {
+        return true;
+    }
+    return false;
+}
+
 class GameModel : public stf::smv::BaseModel
 {
 public:
+
     GameModel() : stf::smv::BaseModel() {
         for(auto it = board.begin(); it != board.end(); ++it) {
-            *it = new BoardCell();
+            *it = BoardCellFactory::boardCell.create();
         }
-        board.at(0) = new CheckerCell();
-        board.at(1) = new QueenCheckerCell();
+        board.at(0) = BoardCellFactory::checkerCell.create();
+        board.at(1) = BoardCellFactory::queenCheckerCell.create();
     }
 
     BoardCell* get(const stf::Vec2d& pos) { return board.at(Size.x * pos.y + pos.x); }
+    void place(const Selector& s) { board.at(Size.x * s.pos.y + s.pos.x) = s.cell; }
 
     stf::smv::IView *keyEventsHandler(stf::smv::IView *sender, const int key)
     {
@@ -71,7 +96,20 @@ public:
         case 's': if(m_cursor.selectorCell.pos.y < Size.y-1) m_cursor.selectorCell.pos += stf::Vec2d(0,1); break;
         case 'd': if(m_cursor.selectorCell.pos.x < Size.x-1) m_cursor.selectorCell.pos += stf::Vec2d(1,0); break;
         case 'q': return nullptr;
-//        case ' ': return put(sender);
+        case ' ':
+            BoardCell *cell = get(m_cursor.selectorCell.pos);
+            Selector &sc = m_cursor.selectorCell;
+            Selector &dc = m_cursor.destinationCell;
+            if(sc.cell == dc.cell && *sc.cell != BoardCell()) {
+                if(cell->onPlacementHandler(board, m_cursor)) {
+                    place(sc);
+                    sc.cell = dc.cell = BoardCellFactory::boardCell.create();
+                }
+            } else if(*cell != BoardCell() ) {
+                m_cursor.destinationCell.pos = m_cursor.selectorCell.pos;
+                m_cursor.destinationCell.cell = m_cursor.selectorCell.cell = cell;
+            }
+            break;
         }
         return sender;
     }
@@ -96,17 +134,20 @@ public:
                 renderer.drawPixel({x*3+1, y+2}, model->get({x,y})->view());
             }
         }
+        if(*model->m_cursor.destinationCell.cell != BoardCell()){
+            renderer.drawPixel({model->m_cursor.destinationCell.pos.x * 3 + 0, model->m_cursor.destinationCell.pos.y + 2}, '{');
+            renderer.drawPixel({model->m_cursor.destinationCell.pos.x * 3 + 2, model->m_cursor.destinationCell.pos.y + 2}, '}');
+        }
 
         renderer.drawPixel({model->m_cursor.selectorCell.pos.x * 3 + 0, model->m_cursor.selectorCell.pos.y + 2}, '[');
         renderer.drawPixel({model->m_cursor.selectorCell.pos.x * 3 + 2, model->m_cursor.selectorCell.pos.y + 2}, ']');
 
-        renderer.drawPixel({model->m_cursor.destinationCell.pos.x * 3 + 0, model->m_cursor.destinationCell.pos.y + 2}, '{');
-        renderer.drawPixel({model->m_cursor.destinationCell.pos.x * 3 + 2, model->m_cursor.destinationCell.pos.y + 2}, '}');
     }
 };
 
 class Game : public stf::Window
 {
+    int n = 0;
     GameModel gameModel = GameModel();
     GameView gameView = GameView(&gameModel);
     stf::smv::IView *currentView = &gameView;
@@ -116,6 +157,7 @@ class Game : public stf::Window
         if(currentView == nullptr)
             return false;
         currentView->show(renderer);
+//        return n++ == 1 ? false : true;
         return true;
     }
 
@@ -132,5 +174,6 @@ class Game : public stf::Window
 
 int main()
 {
-    return Game().run();
+    Game().run();
+//    _cellAlloc.info();
 }
