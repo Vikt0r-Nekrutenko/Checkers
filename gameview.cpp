08a8a294ef3783bcee0dec03b5b8020fd7faa6b5
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-01 09:00:30 +0200

    018

--- gameview.cpp
+++ gameview.cpp
@@ -13,7 +13,11 @@ void GameView::show(stf::Renderer &renderer)
     GameModel *model = static_cast<GameModel*>(m_model);
     for(int y = 0; y < model->board.Size.y; ++y) {
         for(int x = 0; x < model->board.Size.x; ++x) {
-            renderer.drawPixel({x*3+1, y+2}, model->board[{x,y}]->view());
+            auto c = model->board[{x,y}];
+            if(c == GameBoard::emptyCell())
+                renderer.drawPixel({x*3+1, y+2}, '_');
+            else
+                renderer.drawPixel({x*3+1, y+2}, c->view());
         }
     }
 //    if(*model->m_cursor.selectedCell.cell != EmptyCheckerCell()){
