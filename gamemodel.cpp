0abfdff483512b7ba77eae7acdef2728a5063fa7
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-01 19:15:00 +0200

    030

--- gamemodel.cpp
+++ gamemodel.cpp
@@ -1,1 +1,49 @@
 #include "gamemodel.hpp"
+
+GameModel::GameModel() : stf::smv::BaseModel()
+{
+    board.place({2,0}, GameBoard::whiteChecker());
+    board.place({3,1}, GameBoard::blackChecker());
+    board.place({5,3}, GameBoard::blackChecker());
+}
+
+BoardCell *GameModel::opponent() const {
+    if (player == GameBoard::blackPlayer())
+        return GameBoard::whitePlayer();
+    else
+        return GameBoard::blackPlayer();
+}
+
+stf::smv::IView *GameModel::put(stf::smv::IView *sender)
+{
+    BoardCell *cell = board.getSelectableCell(cursor);
+
+    if(!cursor.cursorIsEmpty() && cursor.selectableCell.cell->onPlacementHandler(this, cursor)) {
+        if(board.objectIsInBlackZone(cursor.selectableCell.pos))
+            board.place(cursor.selectableCell.pos, GameBoard::whiteQueen());
+        else if(board.objectIsInWhiteZone(cursor.selectableCell.pos))
+            board.place(cursor.selectableCell.pos, GameBoard::blackQueen());
+        else
+            board.place(cursor.selectableCell.pos, cursor.selectableCell.cell);
+        cursor.reset();
+        player = opponent();
+    } else if(cell->color() == player->color()) {
+        cursor.select(cell);
+    }
+
+    return sender;
+}
+
+stf::smv::IView *GameModel::keyEventsHandler(stf::smv::IView *sender, const int key)
+{
+    switch (key)
+    {
+    case 'w': if(cursor.selectableCell.pos.y > 0) cursor.selectableCell.pos -= stf::Vec2d(0,1); break;
+    case 'a': if(cursor.selectableCell.pos.x > 0) cursor.selectableCell.pos -= stf::Vec2d(1,0); break;
+    case 's': if(cursor.selectableCell.pos.y < board.Size.y-1) cursor.selectableCell.pos += stf::Vec2d(0,1); break;
+    case 'd': if(cursor.selectableCell.pos.x < board.Size.x-1) cursor.selectableCell.pos += stf::Vec2d(1,0); break;
+    case 'q': return nullptr;
+    case ' ': return put(sender);
+    }
+    return sender;
+}
--- gamemodel.hpp
+++ gamemodel.hpp
@@ -8,55 +8,11 @@
 class GameModel : public stf::smv::BaseModel
 {
 public:
-    GameModel() : stf::smv::BaseModel()
-    {
-        board.place({2,0}, GameBoard::whiteChecker());
-        board.place({3,1}, GameBoard::blackChecker());
-        board.place({5,3}, GameBoard::blackChecker());
-    }
+    GameModel();
 
-    BoardCell *opponent() const {
-        if (player == GameBoard::blackPlayer())
-            return GameBoard::whitePlayer();
-        else
-            return GameBoard::blackPlayer();
-    }
-
-    stf::smv::IView* put(stf::smv::IView *sender)
-    {
-        BoardCell *cell = board.getSelectableCell(cursor);
-        Selector &sc = cursor.selectableCell;
-        Selector &dc = cursor.selectedCell;
-
-        if(!cursor.cursorIsEmpty() && sc.cell->onPlacementHandler(this, cursor)) {
-            if(board.objectIsInBlackZone(cursor.selectableCell.pos))
-                board.place(cursor.selectableCell.pos, GameBoard::whiteQueen());
-            else if(board.objectIsInWhiteZone(cursor.selectableCell.pos))
-                board.place(cursor.selectableCell.pos, GameBoard::blackQueen());
-            else
-                board.place(cursor.selectableCell.pos, cursor.selectableCell.cell);
-            cursor.reset();
-            player = opponent();
-        } else if(cell->color() == player->color()) {
-            cursor.select(cell);
-        }
-
-        return sender;
-    }
-
-    stf::smv::IView *keyEventsHandler(stf::smv::IView *sender, const int key) final
-    {
-        switch (key)
-        {
-        case 'w': if(cursor.selectableCell.pos.y > 0) cursor.selectableCell.pos -= stf::Vec2d(0,1); break;
-        case 'a': if(cursor.selectableCell.pos.x > 0) cursor.selectableCell.pos -= stf::Vec2d(1,0); break;
-        case 's': if(cursor.selectableCell.pos.y < board.Size.y-1) cursor.selectableCell.pos += stf::Vec2d(0,1); break;
-        case 'd': if(cursor.selectableCell.pos.x < board.Size.x-1) cursor.selectableCell.pos += stf::Vec2d(1,0); break;
-        case 'q': return nullptr;
-        case ' ': return put(sender);
-        }
-        return sender;
-    }
+    BoardCell *opponent() const;
+    stf::smv::IView* put(stf::smv::IView *sender);
+    stf::smv::IView *keyEventsHandler(stf::smv::IView *sender, const int key) final;
 
     GameBoard board = GameBoard();
     Cursor cursor = Cursor();
