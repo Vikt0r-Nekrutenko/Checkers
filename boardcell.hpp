cfac0c8cf1de2f2b734ca0c92b65363d06979762
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-11-04 09:21:26 +0200

    053

--- boardcell.hpp
+++ boardcell.hpp
@@ -55,6 +55,7 @@ public:
 class Checker : virtual public BoardCell
 {
 public:
+    uint8_t view() const override { return 'o'; }
     GameTurn* takeNextTurn(GameModel *model, const Cursor &cursor) override;
     GameTurn* isNextTurnAreAttack(GameModel *, const Cursor&, const stf::Vec2d&, const stf::Vec2d&) override;
 
@@ -84,7 +85,6 @@ public:
         rMoveFw   = {+1,+1}, lMoveFw   = {-1,+1};
         rAttackFw = {+2,+2}, lAttackFw = {-2,+2};
     }
-    uint8_t view() const override { return 'w'; }
 };
 
 class BChecker : public Checker, public BlackPlayer
@@ -95,7 +95,6 @@ public:
         rMoveFw   = {+1,-1}, lMoveFw   = {-1,-1};
         rAttackFw = {+2,-2}, lAttackFw = {-2,-2};
     }
-    uint8_t view() const override { return 'b'; }
 };
 
 class WQueen : public Queen, public WhitePlayer {};
